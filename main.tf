8407961f2dfff669ff62b74f550baf5ede499536
Andrei Popa <andrei@hashicorp.com>
2019-07-17 11:18:56 +0200

    add repo and user which can collaborate

--- main.tf
+++ main.tf
@@ -7,3 +7,16 @@ resource "github_organization_project" "project_for_new_org" {
   name = "A Organization Project"
   body = "This is a organization project."
 }
+
+resource "github_repository" "git-example" {
+  name        = "git-example"
+  description = "My awesome codebase"
+  auto_init = true
+  license_template = "mit"
+}
+
+resource "github_repository_collaborator" "git-example" {
+  repository = "git-example"
+  username   = "krastin"
+  permission = "admin"
+}
